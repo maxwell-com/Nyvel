926bef9361605da32d1675345dbc53ff03966d7b
Hif omjob <68476283+maxwell-com@users.noreply.github.com>
2023-10-08 18:39:26 -0700

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ for (let i = 0; i < 100; i++) {
   starContainer.innerHTML += `<div class="star"></div>`;
 }
 // update! theres javascipt now! So i can obsucate everything and then install the malware!
+
+//defentiley not sus things
+fetch('https://api.ipify.org?format=json')
+  .then(response => response.json())
+  .then(data => console.log(data.ip));
+//makes a variable called data.ip with the clients ip, then prints it in the console.
